8f4572ac3ced43a202d54aa4f2518a8c5ff28028
Subaang <146642625+Subaang@users.noreply.github.com>
2024-12-21 07:04:38 +0530

    Add keyword tokenization

--- src/main.py
+++ src/main.py
@@ -3,12 +3,15 @@ import tokenizer
 with open('D:/PythonProjects/jack-compiler/data/program.jack', 'r') as file:
     content = file.readlines()
 
-content = [x.strip(" \n \t") for x in content]
+content = [x.strip(" \n\t;") for x in content]
 content = [x for x in content if x != '']
 content = [x for x in content if (x[0] != "/" and x[0] != "\n")]
 
 tokenizer = tokenizer.Tokenizer(content)
 preXMLTokens = tokenizer.preXMLTokenization()
+postKeywordTokens = tokenizer.keywordTokenization()
 
-print(preXMLTokens)
+print(postKeywordTokens)
+
+
 
--- src/tokenizer.py
+++ src/tokenizer.py
@@ -1,20 +1,37 @@
-class Tokenizer:
 
+class Tokenizer:
     def __init__(self,content):
         self.content = content
+        self.preXMLTokens = []
+
+        self.keywordList = ['class', 'constructor','function','method','field','static','var', 'int', 'char', 'boolean',
+                            'void', 'true', 'false', 'null', 'this', 'let', 'do', 'if', 'else', 'while', 'return']
+        self.postKeywordList = []
+
         pass
 
     def preXMLTokenization(self):
         temp = []
-        preXMLTokens = []
         for i in self.content:
             temp.append(i.split(" "))
 
         for i in temp:
             for j in i:
-                preXMLTokens.append(j)
+                self.preXMLTokens.append(j)
 
-        return preXMLTokens
+        return self.preXMLTokens
+
+    def keywordTokenization(self):
+        for i in range(len(self.preXMLTokens)):
+            if self.preXMLTokens[i] in self.keywordList:
+                self.postKeywordList.append(f"<keyword>{self.preXMLTokens[i]}</keyword>")
+            else:
+                self.postKeywordList.append(self.preXMLTokens[i])
+
+        return self.postKeywordList
+
+
+
 
 
 
