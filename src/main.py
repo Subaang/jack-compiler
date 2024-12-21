2b825a639c7dbe1fa86f16490903c35854212dbc
Subaang <146642625+Subaang@users.noreply.github.com>
2024-12-22 02:48:15 +0530

    Complete Tokenizer

--- src/main.py
+++ src/main.py
@@ -1,17 +1,34 @@
 import tokenizer
 
-with open('D:/PythonProjects/jack-compiler/data/program.jack', 'r') as file:
+with open('D:/PythonProjects/jack-compiler/data/test.jack', 'r') as file:
     content = file.readlines()
 
-content = [x.strip(" \n\t;") for x in content]
+content = [x.strip(" \n\t") for x in content]
 content = [x for x in content if x != '']
 content = [x for x in content if (x[0] != "/" and x[0] != "\n")]
 
-tokenizer = tokenizer.Tokenizer(content)
-preXMLTokens = tokenizer.preXMLTokenization()
-postKeywordTokens = tokenizer.keywordTokenization()
+tokenizer1 = tokenizer.Tokenizer()
+preXMLTokens = tokenizer1.preXMLTokenization(content)
+symbolAndKeywordTokens = tokenizer1.symbolAndKeywordTokenization(preXMLTokens)
+intTokens = tokenizer1.intConstTokenization(symbolAndKeywordTokens)
+stringTokens = tokenizer1.stringConstTokenization(intTokens)
+identifierTokens = tokenizer1.identifierTokenization(stringTokens)
 
-print(postKeywordTokens)
+tokenizedXML = identifierTokens.copy()
+
+with open('D:/PythonProjects/jack-compiler/data/tokens.xml', 'w') as file:
+    file.write('<tokens>\n')
+    for i in tokenizedXML:
+        file.write(i)
+        file.write('\n')
+    file.write('</tokens>')
+
+
+if not tokenizer1.verifyXML(identifierTokens):
+    print("PROBLEM ---------------------------")
+
+
+
 
 
 
--- src/tokenizer.py
+++ src/tokenizer.py
@@ -1,38 +1,120 @@
+from itertools import filterfalse
+
 
 class Tokenizer:
-    def __init__(self,content):
-        self.content = content
-        self.preXMLTokens = []
-
+    def __init__(self):
         self.keywordList = ['class', 'constructor','function','method','field','static','var', 'int', 'char', 'boolean',
                             'void', 'true', 'false', 'null', 'this', 'let', 'do', 'if', 'else', 'while', 'return']
-        self.postKeywordList = []
+        self.symbolList = ['{','}','(',')','[',']','.',',','+','-','*','/','&','|','<','>','=',"~",';']
 
-        pass
+    def preXMLTokenization(self,content):
+        pre_res = []
+        for i in content:
+            pre_res += i.split(" ")
 
-    def preXMLTokenization(self):
-        temp = []
-        for i in self.content:
-            temp.append(i.split(" "))
+        for i in range(len(pre_res)):
+            temp = []
+            word = ""
+            for j in pre_res[i]:
+                if j in self.symbolList:
+                    if word != "":
+                        temp.append(word)
+                        word = ""
 
-        for i in temp:
-            for j in i:
-                self.preXMLTokens.append(j)
+                    temp.append(j)
+                else:
+                    word += j
+            if word != "":
+                temp.append(word)
 
-        return self.preXMLTokens
+            pre_res[i] = temp
 
-    def keywordTokenization(self):
-        for i in range(len(self.preXMLTokens)):
-            if self.preXMLTokens[i] in self.keywordList:
-                self.postKeywordList.append(f"<keyword>{self.preXMLTokens[i]}</keyword>")
+        pre_res = [j for sub in pre_res for j in sub]
+
+        res = []
+        i = 0
+        while i < len(pre_res):
+            if pre_res[i][0] == '"':
+                temp = ""
+                temp += pre_res[i]
+                temp += " "
+                i += 1
+                while pre_res[i][0] != '"':
+                    temp += pre_res[i]
+                    temp += " "
+                    i += 1
+
+                if temp == "":
+                    res.append(temp)
+                else:
+                    res.append(temp)
+
             else:
-                self.postKeywordList.append(self.preXMLTokens[i])
+                res.append(pre_res[i])
 
-        return self.postKeywordList
+            i += 1
+        return res
 
+    def symbolAndKeywordTokenization(self,content):
+        res = []
+        for i in range(len(content)):
+            if content[i] in self.keywordList:
+                res.append(f"<keyword>{content[i]}</keyword>")
 
+            elif content[i] in self.symbolList:
+                if content[i] == '<':
+                    res.append(f"<symbol>&lt;</symbol>")
+                elif content[i] == '>':
+                    res.append(f"<symbol>&gt;</symbol>")
+                elif content[i] == '"':
+                    res.append(f"<symbol>&quot;</symbol>")
+                    print("CHECK THIS. HOW DID THIS HAPPEN -----------------------------------------")
+                elif content[i] == '&':
+                    res.append(f"<symbol>&amp;</symbol>")
+                else:
+                    res.append(f"<symbol>{content[i]}</symbol>")
 
+            else:
+                res.append(content[i])
 
+        return res
 
+    def intConstTokenization(self,content):
+        res = []
+        for i in range(len(content)):
+            try:
+                int(content[i])
+                res.append("<intConst>" + content[i] + "</intConst>")
+            except ValueError:
+                res.append(content[i])
+
+        return res
+
+    def stringConstTokenization(self,content):
+        res = []
+        for i in range(len(content)):
+            if content[i][0] == '"':
+                res.append("<stringConst>" + content[i][1:] + "</stringConst>")
+            else:
+                res.append(content[i])
+
+        return res
+
+    def identifierTokenization(self,content):
+        res = []
+        for i in range(len(content)):
+            if content[i][0].isalpha() or content[i][0].isalpha():
+                res.append("<identifier>" + content[i] + "</identifier>")
+            else:
+                res.append(content[i])
+
+        return res
+
+    def verifyXML(self,content):
+        for i in content:
+            if i[0] != "<":
+                return False
+
+        return True
 
 
