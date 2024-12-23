8842b861d427b90c02a970134c536ed627bbdcd8
Subaang <146642625+Subaang@users.noreply.github.com>
2024-12-24 00:09:42 +0530

    Add multiline comment support

--- src/main.py
+++ src/main.py
@@ -7,6 +7,22 @@ for filePath in os.listdir():
     with open(filePath, 'r') as file:
         content = file.readlines()
 
+    multi_line_comment_flag = False # Removes multiline comments
+    to_remove = []
+    for i in range(len(content)):
+        if "/*" in content[i] and "*/" in content[i]:
+            to_remove.append(i)
+        elif "/*" in content[i]:
+            multi_line_comment_flag = True
+        elif "*/" in content[i]:
+            multi_line_comment_flag = False
+            to_remove.append(i)
+        if multi_line_comment_flag:
+            to_remove.append(i)
+
+    for index in sorted(to_remove, reverse=True):
+        del content[index]
+
     for i in range(len(content)):  # Removes inline comments
         if '//' in content[i]:
             content[i] = content[i][:content[i].index('//')]
@@ -14,8 +30,8 @@ for filePath in os.listdir():
     content = [x.strip(" \n\t") for x in content]
     content = [x for x in content if x != '']
     content = [x for x in content if (x[0] != "/" and x[0] != "\n")]
-
     print(content)
+
 
     tokenizer1 = tokenizer.Tokenizer(content)
     tokenizedXML = tokenizer1.tokenize()
