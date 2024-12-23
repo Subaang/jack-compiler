d8fdb3dc5ec2b9e86b400b49df062c623eb7ca22
Subaang <146642625+Subaang@users.noreply.github.com>
2024-12-23 08:10:51 +0530

    Implement program structure and statements compilation

--- src/compiler.py
+++ src/compiler.py
@@ -23,7 +23,7 @@ class CompilationEngine:
             if tokenizer.keyword(token) not in terminal_list:
                error()
 
-        elif tokenType == "IDENTIFIER":
+        elif tokenType == "IDENTIFIER": #To handle the case of typeList, this must be below KEYWORD
             if terminal_list[0] != "IDENTIFIER":
                 error()
 
@@ -48,34 +48,38 @@ class CompilationEngine:
         self.eat(["IDENTIFIER"])
         self.eat(["{"])
 
-        #These 2 can come in any order multiple times
+        # These 2 can come in any order multiple times or not appear at all
+
         while True:
-            if tokenizer.tokenType(self.tokens[self.i]) == "SYMBOL" and tokenizer.symbol(self.tokens[self.i]) == "}":
+            if tokenizer.tokenType(self.tokens[self.i]) == "SYMBOL": #Not checking which symbol it is because we eat non-} and get exit()
                 break
 
-            self.compileClassVarDec()
+            if tokenizer.tokenType(self.tokens[self.i]) == "KEYWORD" and tokenizer.keyword(self.tokens[self.i]) in ["field", "static","constructor","function","method"]:
+                if tokenizer.keyword(self.tokens[self.i]) in ["field", "static"]:
+                    self.compileClassVarDec()
+                elif tokenizer.keyword(self.tokens[self.i]) in ["constructor","function","method"]:
+                    self.compileSubroutineDec()
+                else:
+                    error()
 
-            if tokenizer.tokenType(self.tokens[self.i]) == "SYMBOL" and tokenizer.symbol(self.tokens[self.i]) == "}":
-                break
-
-            self.compileSubroutineDec()
+            if tokenizer.tokenType(self.tokens[self.i]) not in ["SYMBOL","KEYWORD"]: # Necessary to break out of infinite loop
+                error()
 
         self.eat(["}"])
         self.compiled.append("</class>")
 
     def compileClassVarDec(self):
-        while tokenizer.tokenType(self.tokens[self.i]) == "KEYWORD" and tokenizer.keyword(self.tokens[self.i]) in ["field", "static"]:
-            self.compiled.append("<classVarDec>")
-            self.eat(["field","static"])
-            self.eat(self.typeList)
-            self.eat(["IDENTIFIER"])
+        self.compiled.append("<classVarDec>")
+        self.eat(["field","static"])
+        self.eat(self.typeList)
+        self.eat(["IDENTIFIER"])
 
-            while (tokenizer.tokenType(self.tokens[self.i]) == "SYMBOL" and tokenizer.symbol(self.tokens[self.i]) == ";") is False:
-                self.eat([","])
-                self.eat(["IDENTIFIER"])
+        while (tokenizer.tokenType(self.tokens[self.i]) == "SYMBOL" and tokenizer.symbol(self.tokens[self.i]) == ";") is False:
+             self.eat([","])
+             self.eat(["IDENTIFIER"])
 
-            self.eat([";"])
-            self.compiled.append("</classVarDec>")
+        self.eat([";"])
+        self.compiled.append("</classVarDec>")
 
     def compileSubroutineDec(self):
         self.compiled.append("<subroutineDec>")
@@ -97,11 +101,143 @@ class CompilationEngine:
             self.eat([","])
 
         self.eat(")")
-        self.eat("{")
-        #self.compileSubroutineBody()
-        self.eat("}")
+        self.compileSubroutineBody()
         self.compiled.append("</subroutineDec>")
 
+    def compileSubroutineBody(self):
+        self.compiled.append("<subroutineBody>")
+        self.eat("{")
+
+        # These 2 can come in any order multiple times or not appear at all
+        while True:
+            if tokenizer.tokenType(self.tokens[self.i]) == "SYMBOL":
+                break
+
+            if tokenizer.tokenType(self.tokens[self.i]) == "KEYWORD" and tokenizer.keyword(self.tokens[self.i]) in ['var','let','if','while','do','return']:
+                if tokenizer.keyword(self.tokens[self.i]) == "var":
+                    self.compileVarDec()
+                elif tokenizer.keyword(self.tokens[self.i]) in ['let','if','while','do','return']:
+                    self.compileStatements()
+                else:
+                    error()
+
+            if tokenizer.tokenType(self.tokens[self.i]) not in ["SYMBOL","KEYWORD"]:
+                error()
+
+        self.eat("}")
+        self.compiled.append("</subroutineBody>")
+
+    def compileVarDec(self):
+        self.compiled.append("<varDec>")
+
+        self.eat(["var"])
+        self.eat(self.typeList)
+        self.eat(["IDENTIFIER"])
+        while (tokenizer.tokenType(self.tokens[self.i]) == "SYMBOL" and tokenizer.symbol(self.tokens[self.i]) == ";") is False:
+            self.eat([","])
+            self.eat(["IDENTIFIER"])
+
+        self.eat([";"])
+
+        self.compiled.append("</varDec>")
+
+    def compileStatements(self):
+        self.compiled.append("<statements>")
+
+        while True:
+            if tokenizer.keyword(self.tokens[self.i]) == "let":
+                self.compileLet()
+            elif tokenizer.keyword(self.tokens[self.i]) == "if":
+                self.compileIf()
+            elif tokenizer.keyword(self.tokens[self.i]) == "while":
+                self.compileWhile()
+            elif tokenizer.keyword(self.tokens[self.i]) == "do":
+                self.compileDo()
+            elif tokenizer.keyword(self.tokens[self.i]) == "return":
+                self.compileReturn()
+            else:
+                break
+
+        self.compiled.append("</statements>")
+
+    def compileLet(self):
+        self.compiled.append("<letStatement>")
+
+        self.eat(["let"])
+        self.eat(["IDENTIFIER"])
+
+        if tokenizer.tokenType(self.tokens[self.i]) == "SYMBOL" and tokenizer.symbol(self.tokens[self.i]) == "=":
+            self.eat(["="])
+            #self.compileExpression()
+        else:
+            self.eat(["["])
+            # self.compileExpression()
+            self.eat(["]"])
+            self.eat(["="])
+            # self.compileExpression()
+
+        self.eat(";")
+        self.compiled.append("</letStatement>")
+
+    def compileIf(self):
+        self.compiled.append("<ifStatement>")
+
+        self.eat(["if"])
+        self.eat(["IDENTIFIER"])
+        self.eat(["("])
+        #self.compileExpression()
+        self.eat([")"])
+        self.eat(["{"])
+        self.compileStatements()
+        self.eat(["}"])
+
+        if tokenizer.tokenType(self.tokens[self.i]) == "SYMBOL" and tokenizer.symbol(self.tokens[self.i]) == ";":
+           pass
+        else:
+            self.eat(["else"])
+            self.eat(["{"])
+            self.compileStatements()
+            self.eat(["}"])
+
+        self.compiled.append("</ifStatement>")
+
+    def compileWhile(self):
+        self.compiled.append("<whileStatement>")
+
+        self.eat(["while"])
+        self.eat(["IDENTIFIER"])
+        self.eat(["("])
+        # self.compileExpression()
+        self.eat([")"])
+        self.eat(["{"])
+        self.compileStatements()
+        self.eat(["}"])
+
+        self.compiled.append("</whileStatement>")
+
+    def compileDo(self):
+        self.compiled.append("<doStatement>")
+
+        self.eat(["do"])
+        self.compileSubroutineDec()
+        self.eat([";"])
+
+        self.compiled.append("</doStatement>")
+
+    def compileReturn(self):
+        self.compiled.append("<returnStatement>")
+
+        self.eat(["return"])
+
+        if tokenizer.tokenType(self.tokens[self.i]) == "SYMBOL" and tokenizer.symbol(self.tokens[self.i]) == ";":
+            pass
+        else:
+            pass
+            #self.compileExpression()
+
+        self.eat([";"])
+
+        self.compiled.append("</returnStatement>")
 
 
 
--- src/main.py
+++ src/main.py
@@ -4,9 +4,15 @@ import compiler
 with open('D:/PythonProjects/jack-compiler/data/test.jack', 'r') as file:
     content = file.readlines()
 
+for i in range(len(content)): #Removes inline comments
+    if '//' in content[i]:
+        content[i] = content[i][:content[i].index('//')]
+
 content = [x.strip(" \n\t") for x in content]
 content = [x for x in content if x != '']
 content = [x for x in content if (x[0] != "/" and x[0] != "\n")]
+
+
 
 tokenizer1 = tokenizer.Tokenizer(content)
 tokenizedXML = tokenizer1.tokenize()
